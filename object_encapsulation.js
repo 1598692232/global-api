e18343d90eb8509b90fc49a04a1a88ac4a4371e7
彭志亮 <EX-pengzhiliang001@pingan.com.cn>
2017-06-22 10:15:10 +0800

    tijao

---
+++ object_encapsulation.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ex-pengzhiliang001 on 2017-06-22.
+ */
+(function (factory,undefined) {
+	if(typeof module === 'object' && typeof module.exports == 'object'){
+		module.exports = factory();
+	}else{
+		window.ObjectEncapsulation = factory();
+	}
+})(function () {
+
+})
